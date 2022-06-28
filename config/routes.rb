78775090e79d924e942282a752ff68790e7357fd
osvaldo <iosvaldo.app@gmail.com>
2022-06-28 10:38:02 -0400

    commit tree 92b2ccd8a2f799ac56e7aae41bdc9ec8b54edb96

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get '/cheeses', to: 'cheeses#index'
-  get '/cheeses/:id', to: 'cheeses#show'
+
+  resources :cheeses, only: [:index, :show]
+  # get '/cheeses', to: 'cheeses#index'
+  # get '/cheeses/:id', to: 'cheeses#show'
 end
